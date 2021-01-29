450fde0a81fe10dad01ef6f7af47b94107b95a2d
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-01-29 13:21:08 +0200

    added functions to the code
    reorganized the code

--- src/main.js
+++ src/main.js
@@ -1,26 +1,56 @@
+function addTask() {
+  let inputText = document.getElementById("text-input");
+  let prioritySelector = document.getElementById("priority-selector");
+  let counter = document.getElementById("counter");
+
+  let newTask = {
+    Text: inputText.value,
+    Date: new Date(),
+    Priority: prioritySelector.value,
+    IsDone: false,
+  };
+  tasks.push(newTask);
+
+  let li = newElem("li");
+  let taskDiv = newElem("div", "todo-container");
+  let taskText = newElem("div", "todo-text");
+  let taskPriority = newElem("div", "todo-priority");
+  let taskDate = newElem("div", "todo-created-at");
+  let doneButton = newElem("button", "done-button");
+
+  taskText.innerText = newTask.Text;
+  taskPriority.innerText = newTask.Priority;
+  taskDate.innerText = newTask.Date;
+  doneButton.innerText = "undone";
+  doneButton.addEventListener("click", function () {
+    isDone(doneButton);
+  });
+
+  taskDiv.append(taskText, taskPriority, taskDate, doneButton);
+  li.append(taskDiv);
+  addedTasks.prepend(li);
+  inputText.value = "";
+  counter.innerText = ++count;
+}
+
+function newElem(type, className) {
+  let elem = document.createElement(type);
+  if (className) elem.className = className;
+  return elem;
+}
+
+function isDone(doneButton) {
+  if (doneButton.innerText === "undone") {
+    doneButton.innerText = "done";
+  } else {
+    doneButton.innerText = "undone";
+  }
+}
+
+let tasks = [];
+let count = 0;
+
 const controlSection = document.getElementById("control-section");
 const addButton = document.getElementById("add-button");
-let inputTask = document.getElementById("task");
 const addedTasks = document.getElementById("List");
-const priority = document.getElementById("priority");
-let counter = 0;
-addButton.addEventListener("click", function () {
-  let li = document.createElement("li");
-  let taskDiv = document.createElement("div");
-  taskDiv.setAttribute("class", "todo-container");
-  let taskText = document.createElement("div");
-  taskText.setAttribute("class", "todo-text");
-  let taskPriority = document.createElement("div");
-  taskPriority.setAttribute("class", "todo-priority");
-  let taskDate = document.createElement("div");
-  taskDate.setAttribute("class", "todo-created-at");
-  taskText.innerText = inputTask.value;
-  taskPriority.innerText = priority.value;
-  let date = new Date();
-  taskDate.innerText = date.toString();
-  taskDiv.append(taskDate, taskPriority, taskText);
-  li.append(taskDiv);
-  addedTasks.append(li);
-  inputTask.value = "";
-  count++;
-});
+addButton.addEventListener("click", addTask);
